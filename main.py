719481faf008bb1b9303877c45b1e67854ea4191
Cranckers <crancker@gmail.com>
2021-09-25 22:22:52 -0300

    omo Ler Textos Dentro de Imagens com Python [Introdução ao OpenCV e Tesseract]

---
+++ main.py
@@ -0,0 +1,17 @@
+import pytesseract
+import cv2
+
+# links uteis:
+# corrigir instalação windows: https://stackoverflow.com/questions/50951955/pytesseract-tesseractnotfound-error-tesseract-is-not-installed-or-its-not-i
+# instalar outra língua: https://github.com/tesseract-ocr/tessdata
+# pegar linguas: print(pytesseract.get_languages())
+
+# CMD que vai ler a imagem e o nome da imagem
+imagem = cv2.imread("print_magalu.JPG")
+
+path = r"C:\Users\Lucas Oliveira\AppData\Local\Programs\Tesseract-OCR"
+
+pytesseract.pytesseract.tesseract_cmd = path + r'\tesseract.exe'
+# Este CMD falar para tesseract converter a imagem em texto
+texto = pytesseract.image_to_string(imagem, lang="por")
+print(texto)
